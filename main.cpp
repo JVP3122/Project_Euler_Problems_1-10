90fb12b1c921b040238548b17e845abf2d17654f
JVP3122 <jeff.pohlmeyer@gmail.com>
2016-03-08 23:36:32 -0500

    Problem 3
    https://projecteuler.net/problem=3

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 #include<math.h>
+#include<vector>
+#include<limits>
 
 int P1(int, int, int);
 int P2(int);
-int P3(int);
+int P3(long long int);
 bool prime(int);
 
 void main() {
 	using namespace std;
-	//	cout << "The answer to problem 1 is " << P1(3, 5, 1000) << endl;
-	//	cout << "The answer to problem 2 is " << P2(4000000) << endl;
-	cout << prime(169) << endl;
+	cout << "The answer to problem 1 is: " << P1(3, 5, 1000) << endl;
+	cout << "The answer to problem 2 is: " << P2(4000000) << endl;
+	cout << "The answer to problem 3 is: " << P3(600851475143) << endl;
 }
 
 // Problem 1 - Find the sum of all multiples of 3 or 5 below 1000
@@ -39,13 +41,27 @@ int P2(int maxVal) {
 	return sumVal;
 }
 
-/*int P3(int result) {
-int tempVal = 0;
+// Problem 3 - What is the largest prime factor of the number 600851475143 ?
+int P3(long long int maxVal) {
+	int tempVal = 1, iterCount = 0;
+	std::vector<int> resultsVector;
+	while (tempVal < sqrt(maxVal)) {
+		if ((maxVal % tempVal == 0) && prime(tempVal) == true)
+			resultsVector.push_back (tempVal);
+		tempVal++;
+	}
+	return resultsVector[resultsVector.size()-1];
 
-if (result % tempVal == 0) {
-
+/*	This section was used to make sure that all prime factors were being properly calculated*/
+/*	for (unsigned int i = 0; i < resultsVector.size(); i++) {
+		if (i < resultsVector.size() - 1) {
+			std::cout << resultsVector[i] << ", ";
+		}
+		else {
+			std::cout << resultsVector[i] << std::endl;
+		}
+	} */
 }
-}*/
 
 // Function to determine if a number is a prime.  I don't yet know how to put this into its own source file and create a separate header file so it is left here.
 bool prime(int inputVal) {
@@ -63,7 +79,7 @@ bool prime(int inputVal) {
 				break;
 			}
 			divisor += 2;
-			cout << divisor << "\t" << primeVal << "\t" << inputVal << endl;
+//			cout << divisor << "\t" << primeVal << "\t" << inputVal << endl;
 		}
 		return primeVal;
 	}
